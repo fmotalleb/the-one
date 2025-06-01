3340bd922ba8565818105f48ad7f335f959c6c7c
FMotalleb <m.falahnejad@dornica.net>
2025-06-01 12:03:42 +0330

    feat: enhance event handling with improved logging and subscription management in WaitForService and WaitForEngine functions

--- controller/controller.go
+++ controller/controller.go
@@ -4,14 +4,29 @@ import (
 	"context"
 	"errors"
 
+	"go.uber.org/zap"
+
 	"github.com/fmotalleb/the-one/config"
+	"github.com/fmotalleb/the-one/logging"
 	"github.com/fmotalleb/the-one/renderer"
 )
 
+var log = logging.LazyLogger("controller")
+
 func Boot(ctx context.Context, cfg *config.Config) error {
+	l := log().Named("Boot")
+	l.Info("booting service controller")
 	// Compile Templates
 	for _, t := range cfg.Templates {
+		tl := l.With(zap.String("src", t.GetSourceDirectory()))
+		tl.Debug(
+			"rendering template directory",
+		)
 		if err := renderer.RenderTemplates(&t); err != nil && t.GetIsFatal() {
+			tl.Error(
+				"failed to render templates",
+				zap.Error(err),
+			)
 			return errors.Join(
 				ErrEngineBoot,
 				err,
@@ -19,5 +34,10 @@ func Boot(ctx context.Context, cfg *config.Config) error {
 		}
 	}
 
+	if len(cfg.Templates) != 0 {
+		l.Debug("finished template rendering")
+	} else {
+		l.Debug("no template was found, ignoring the step")
+	}
 	return nil
 }
--- controller/events.go
+++ controller/events.go
@@ -4,36 +4,120 @@ import (
 	"context"
 	"regexp"
 
+	"go.uber.org/zap"
+
 	"github.com/fmotalleb/the-one/broadcast"
 )
 
-type EventSource uint16
+type (
+	EventSource  uint16
+	EngineState  uint16
+	ServiceState uint16
+)
 
+// Values of this constants are important.
 const (
-	ESEngine  = EventSource(0)
-	ESService = EventSource(1)
+	ESEngine  = EventSource(1)
+	ESService = EventSource(2)
+
+	EngineUp       = EngineState(0b001)
+	EngineShutdown = EngineState(0b010)
+
+	ServiceUp      = ServiceState(0b00001)
+	ServiceAny     = ServiceState(0b00000)
+	ServiceReady   = ServiceState(0b00010)
+	ServiceStarted = ServiceState(0b00100) | ServiceUp
+	ServiceHealthy = ServiceState(0b01000) | ServiceUp
+	ServiceDown    = ServiceState(0b10000)
 )
 
 type Event struct {
-	Source    EventSource
-	Parameter string
+	Source EventSource
+
+	// Engine specific fields
+	EngineState EngineState
+
+	// Service specific fields
+	ServiceState ServiceState
+	ServiceName  string
 }
 
-func WaitFor(
+func WaitForService(
 	ctx context.Context,
 	cast broadcast.Subscription[Event],
-	src EventSource,
-	matcher regexp.Regexp,
+	matcher *regexp.Regexp,
+	state ...ServiceState,
 ) {
-	notifier := cast.Subscribe()
-	for {
-		select {
-		case e := <-notifier:
-			if e.Source == src && matcher.FindIndex([]byte(e.Parameter)) != nil {
-				break
-			}
-		case <-ctx.Done():
-			break
-		}
+	log := log().Named("WaitForService")
+
+	listenOn := ServiceAny
+	if len(state) != 0 {
+		listenOn = state[0]
 	}
+	log.Debug("begin listening", zap.Uint16("listen-on", uint16(listenOn)))
+
+	broadcast.Subscribe(
+		cast,
+		func(c <-chan Event) {
+			for {
+				select {
+				case e := <-c:
+					if e.Source == ESService &&
+						matcher.FindIndex([]byte(e.ServiceName)) != nil &&
+						listenOn&e.ServiceState == listenOn {
+						log.Info("matched service event",
+							zap.String("service", e.ServiceName),
+							zap.Uint16("state", uint16(e.ServiceState)),
+						)
+						return
+					} else {
+						log.Debug("skipped event",
+							zap.String("service", e.ServiceName),
+							zap.Uint16("state", uint16(e.ServiceState)),
+							zap.Any("event", e),
+						)
+					}
+				case <-ctx.Done():
+					log.Warn("context canceled or deadline exceeded")
+					return
+				}
+			}
+		},
+	)
+	log.Debug("finished waiting", zap.Uint16("listen-on", uint16(listenOn)))
+}
+
+func WaitForEngine(
+	ctx context.Context,
+	cast broadcast.Subscription[Event],
+) {
+	log := log().Named("WaitForEngine")
+	listenOn := EngineUp
+	log.Debug("begin listening", zap.Uint16("listen-on", uint16(listenOn)))
+
+	broadcast.Subscribe(
+		cast,
+		func(c <-chan Event) {
+			for {
+				select {
+				case e := <-c:
+					if e.Source == ESEngine && listenOn&e.EngineState == listenOn {
+						log.Info("matched engine event",
+							zap.Uint16("state", uint16(e.EngineState)),
+						)
+						return
+					} else {
+						log.Debug("skipped event",
+							zap.Uint16("state", uint16(e.EngineState)),
+							zap.Any("event", e),
+						)
+					}
+				case <-ctx.Done():
+					log.Warn("context canceled or deadline exceeded")
+					return
+				}
+			}
+		},
+	)
+	log.Debug("finished waiting", zap.Uint16("listen-on", uint16(listenOn)))
 }
