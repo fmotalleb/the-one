a682f75a86d484a20582065ddbe787a70644a00b
FMotalleb <fmotalleb@gmail.com>
2025-07-20 14:29:04 +0330

    added docs

--- config/config.go
+++ config/config.go
@@ -9,5 +9,11 @@ type Config struct {
 }
 
 func (c *Config) BuildServiceGraph() ([]*tree.Node[*Service], error) {
+	// Config need to be sanitized at first
+	// then create the tree
+	// remove lazy nodes with no children
+	// create abstract of runtime
+	// abstract should be absolute and envfile should be loaded into env variables already
+	// abstract will be passed to Boot service directly
 	return tree.NewForest(c.Services)
 }
